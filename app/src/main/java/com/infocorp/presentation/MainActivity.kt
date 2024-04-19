2c5c13fd1e56e0b23f7b077ee325609b22426d7e
maratbelitski <marat.belitckiy2010@yandex.com>
2024-04-19 11:47:53 +0300

    add clean architecture directories

--- app/src/main/java/com/infocorp/presentation/MainActivity.kt
+++ app/src/main/java/com/infocorp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.infocorp
+package com.infocorp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.infocorp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
