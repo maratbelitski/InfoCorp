85a8d419ca040be3a1c20135166c7daaed7c0ea9
maratbelitski <marat.belitckiy2010@yandex.com>
2024-04-21 21:24:31 +0300

    add bottom menu (fix menu items)

--- app/src/main/java/com/infocorp/presentation/MainActivity.kt
+++ app/src/main/java/com/infocorp/presentation/MainActivity.kt
@@ -3,10 +3,16 @@ package com.infocorp.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.infocorp.R
+import com.infocorp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomMenu.background = null
+        binding.bottomMenu.menu.getItem(3).isEnabled = false
+
     }
 }
