8c067c65bc4c40203487c8e6c08c9297c103b830
maratbelitski <marat.belitckiy2010@yandex.com>
2024-05-25 00:41:41 +0300

    setting branch

--- app/src/main/java/com/infocorp/presentation/Constants.kt
+++ app/src/main/java/com/infocorp/presentation/Constants.kt
@@ -4,6 +4,5 @@ enum class Constants(val value:String) {
     DATABASE_NAME("corporation.db"),
     BASE_URL("http://suggestions.dadata.ru/suggestions/api/4_1/rs/"),
     AUTHORIZATION_TOKEN("Authorization: Token 6ebaf8e334bac982c60b1ba2fae777febca0303f")
-    AUTHORIZATION_TOKEN("Authorization: Token 6ebaf8e334bac982c60b1ba2fae777febca0303f")
-    AUTHORIZATION_TOKEN("Authorization: Token 6ebaf8e334bac982c60b1ba2fae777febca0303f")
+
 }
