11b6e1caa48a89b13589e6682d0b08eb6c423360
maratbelitski <marat.belitckiy2010@yandex.com>
2024-05-25 00:33:10 +0300

    setting branch

--- app/src/main/java/com/infocorp/data/network/CorporationFactory.kt
+++ app/src/main/java/com/infocorp/data/network/CorporationFactory.kt
@@ -7,9 +7,6 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 
 class CorporationFactory {
-    companion object {
-//        private const val BASE_URL = "http://suggestions.dadata.ru/suggestions/api/4_1/rs/"
-    }
 
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL.value)
